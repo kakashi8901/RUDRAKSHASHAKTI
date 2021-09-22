2810a06ba8f150452081605943e8ee5338a6d548
Rishav-Chanda <your_email_address@example.com>
2021-09-22 19:53:19 +0530

    made under review screen

--- app/src/main/java/com/rudraksha/rudrakshashakti/Authentication/UnderReview.java
+++ app/src/main/java/com/rudraksha/rudrakshashakti/Authentication/UnderReview.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 
 import com.rudraksha.rudrakshashakti.databinding.ActivityDetailsPageBinding;
@@ -25,14 +26,14 @@ public class UnderReview extends AppCompatActivity implements View.OnClickListen
 
     private void setListeners() {
         binding.sendNow.setOnClickListener(this);
-        binding.textView2.setOnClickListener(this);
+        binding.email.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
        if(view == binding.sendNow){
            openEmailId();
-       }else if(view == binding.textView2){
+       }else if(view == binding.email){
            openEmailId();
        }
     }
@@ -40,9 +41,9 @@ public class UnderReview extends AppCompatActivity implements View.OnClickListen
     private void openEmailId() {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]
-                {"Aadishaktiadmin@rudraksha.org.in"});
+                {"aadishaktiadmin@rudraksha.org.in"});
         intent.putExtra(Intent.EXTRA_SUBJECT, "Verify My Documents For Account Creation RUDRASHAKTI");
-        intent.putExtra(Intent.EXTRA_TEXT, "\n\n1. My Name: \n\n2. My Phone Number: \n\n3. My Aadhar Card Photo: \n\n4. My Pan Card Photo: \n \n");
+        intent.putExtra(Intent.EXTRA_TEXT, "\n\n1. My Colored Photo: \n\n2. My Phone Number: \n\n3. My Whatsapp Number: \n\n4. My UPI Number: \n\n5. My Aadhar Card Photo: \n\n6. My Pan Card: \n\n 7. My Degree Certificate(s): \n\n");
         intent.setType("message/rfc822");
         startActivity(Intent.createChooser(intent,"Choose an Email Client"));
     }
